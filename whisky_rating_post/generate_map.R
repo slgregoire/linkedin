1a5fb32f599a45c670e93ded9d46a7615f5d4ec5
Scott <slgregoire@gmail.com>
2016-05-31 16:32:40 -0400

    finished rough draft of post

--- whisky_rating_post/generate_map.R
+++ whisky_rating_post/generate_map.R
@@ -1,12 +1,17 @@
 library(ggmap)
 library(reshape2)
 
-top_whiskies = read.csv('top_whiskies.csv')
-adjLong = top_whiskies$FinalLong
+top_scotches = read.csv('top_scotches.csv')
+
+adjLong = top_scotches$FinalLong
 adjLong = adjLong + .3
 adjLong[4] = adjLong[4] - .6
-top_whiskies$adjLong = adjLong
+top_scotches$adjLong = adjLong
+
+adjLat = top_scotches$FinalLat
+adjLat[5] = adjLat[5] + .5
+top_scotches$adjLat = adjLat
 
 whiskyMap =  qmap(location = "Scotland", zoom = 6, maptype = "watercolor")
-whiskyMap + geom_point(data = top_whiskies, size=3.5, color='black', mapping=aes(x = FinalLat, y = FinalLong)) +
-  geom_text(data = top_whiskies, color='black', size=5, mapping=aes(x = FinalLat, y = adjLong, label = Distillery))   
+whiskyMap + geom_point(data = top_scotches, size=3.5, color='black', mapping=aes(x = FinalLat, y = FinalLong)) +
+  geom_text(data = top_scotches, color='black', size=5, mapping=aes(x = adjLat, y = adjLong, label = Distillery))   
