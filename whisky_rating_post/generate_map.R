79200fe8aefb9d0798a5eed42621b48b3daa8613
scott <slgregoire@gmail.com>
2016-06-05 18:41:08 -0400

    continued to revise post

--- whisky_rating_post/generate_map.R
+++ whisky_rating_post/generate_map.R
@@ -4,14 +4,26 @@ library(reshape2)
 top_scotches = read.csv('top_scotches_map.csv')
 
 adjLong = top_scotches$FinalLong
-adjLong = adjLong + .3
-adjLong[4] = adjLong[4] - .6
+#Bowmore
+adjLong[1] = adjLong[1] - .3
+#Bruichladdich
+adjLong[2] = adjLong[2] + .3
+#Dalmore
+adjLong[3] = adjLong[3] + .3
 top_scotches$adjLong = adjLong
 
 adjLat = top_scotches$FinalLat
-adjLat[5] = adjLat[5] + .5
+#Old Fettercairn
+adjLat[4] = adjLat[4] + 1.75
+#Old Pulteney
+adjLat[5] = adjLat[5] + 1.45
+#Royal Brackla
+adjLat[6] = adjLat[6] + 1.7
+#Scapa
+adjLat[7] = adjLat[7] + .95
 top_scotches$adjLat = adjLat
 
-whiskyMap =  qmap(location = "Scotland", zoom = 6, maptype = "watercolor")
-whiskyMap + geom_point(data = top_scotches, size=3.5, color='black', mapping=aes(x = FinalLat, y = FinalLong)) +
-  geom_text(data = top_scotches, color='black', size=5, mapping=aes(x = adjLat, y = adjLong, label = Distillery))   
+ 
+whiskyMap =  qmap(location = "Scotland", zoom = 6, maptype = "watercolor") + geom_point(data = top_scotches, size=3.5, color='black', mapping=aes(x = FinalLat, y = FinalLong)) +
+ geom_text(data = top_scotches, color='black', size=5, mapping=aes(x = adjLat, y = adjLong, label = Distillery))   
+print(whiskyMap)
